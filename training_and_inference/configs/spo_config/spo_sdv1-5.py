b847ccf0b3217a7fa9d70123cac9552670883905
Cwyxx <2274402532@qq.com>
2025-08-28 10:10:52 +0800

    2025-8-28-spo_config

--- training_and_inference/configs/spo_config/spo_sdv1-5.py
+++ training_and_inference/configs/spo_config/spo_sdv1-5.py
@@ -6,30 +6,53 @@ def get_config():
 def exp_config():
     config = basic_config()
     
+    ##### SPO-training config #####
+    ###### General ######
+    # random seed for reproducibility.
+    config.seed = 42
+    config.pretrained.model = "runwayml/stable-diffusion-v1-5"
+    config.use_lora = True
+    config.lora_rank = 4
+    ###### Training ######
+    config.train.learning_rate = 6e-5
+    config.sample.guidance_scale = 5.0
+    config.sample_batch_size = 4
+    config.train.learning_rate = 6e-5
+    config.train.adam_weight_decay = 1e-4
+    config.train.gradient_accumulation_steps = 1
+    config.train.max_grad_norm = 1.0
+    config.sample.num_sample_each_step = 4
+    
     ###### Preference Model ######
     # config.preference_model_func_cfg = dict(
     #     type="hpsv2_preference_model_func"
     # )
-    aigi_detector = "dinov2"
-    return_label = True
+    aigi_detector = "drct_clip-sdv14"
+    return_label = False
     config.preference_model_func_cfg = dict(
         type="aigi_detector_preference_model_func",
         aigi_detector=f"{aigi_detector}",
         aigi_detector_path=config.aigi_detector_path_dict[aigi_detector],
         return_label = return_label
     )
+    ###### Compare Function ######
+    config.compare_func_cfg = dict(
+        type="preference_score_compare",
+        threshold=0.0,
+    )
     
     ###### Training ######
+    config.sample.sample_batch_size = 4
     config.sample.num_sample_each_step = 4
     config.train.train_batch_size = 4
     config.train.gradient_accumulation_steps = 1 # total_train_batch_size = 4 * 1 * 4 = 16
-    config.num_epochs = 10
+    config.num_epochs = 4
     
     #### logging ####
     config.train.early_stop_threshold = None
-    config.train.save_and_eval_batch_interval = 250
-    config.wandb_project_name = 'spo-label' if return_label else 'spo'
-    config.logdir = f"/data_center/data2/dataset/chenwy/21164-data/stable_diffusion/stable_diffusion_v1_4/spo_4k/{config.wandb_project_name}"
+    config.train.save_and_eval_batch_interval = 25
+    config.wandb_project_name = "spo-sdv1-5"
+    config.logdir = f"/data_center/data2/dataset/chenwy/21164-data/stable_diffusion/stable_diffusion_v1_5/spo_4k/{config.wandb_project_name}"
     config.run_name = f"{aigi_detector}"
     config.validation_prompts = [ 'a cat.', 'a dog', 'a horse.', 'A bus stopped on the side of the road while people board it.', 'A woman holding a plate of cake in her hand.']
     config.num_validation_images = 1
